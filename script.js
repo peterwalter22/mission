3475253081b2c405b65c85ed117ef2500528a348
peterwalter22 <ukp24003@byui.edu>
2025-09-27 16:36:53 -0600

    updeted the javascript

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
+let selectElem = document.querySelector('select');
 
-const PI = 3.14;
-let radius = 3;
-                  
-const one = 1;
-const two = '2';
-                    
-let course = "CSE131"; //global scope
-if (true) {
-    let student = "John";
-    console.log(course);  //works just fine, course is global
-    console.log(student); //works just fine, it's being accessed within the block
-}
-console.log(course); //works fine, course is global
-console.log(student); //does not work, can't access a block variable outside the block
-                    
+selectElem.addEventListener('change', () => {
+    if (selectElem.value === 'dark') {
+        document.body.classList.add("dark");
+        document.body.classList.remove("light");
+    } else {
+        document.body.classList.add("light");
+        document.body.classList.remove("dark");
+    }
+});
+
+// optional: start in light mode by default
+document.body.classList.add("light");
